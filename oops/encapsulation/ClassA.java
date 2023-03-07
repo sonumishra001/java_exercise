c04a4debf3b16a042e15aa38ed53979fcf1475ba
sonumishra001 <ashutosh.madhipur424@gmail.com>
2023-03-07 19:00:24 +0530

    oops encapsulation

---
+++ oops/encapsulation/ClassA.java
@@ -0,0 +1,23 @@
+public class ClassA {
+    private int studentRollNumber;
+    private String studentName;
+    
+    public int getStudentRollNumber()
+    {
+        return studentRollNumber;
+    }
+    public void setStudentRollNumber(int studentRollNumber)
+    {
+        this.studentRollNumber= studentRollNumber;
+    }
+
+    public String getStudentName()
+    {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName)
+    {
+        this.studentName=studentName;
+    }
+}
